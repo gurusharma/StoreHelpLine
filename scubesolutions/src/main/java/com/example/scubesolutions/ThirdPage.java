f25d9e05716a0c71a163b77c4f46a488c7145eca
Guru Sharma <n01142131@humber.ca>
2017-10-04 19:47:24 -0400

    App layout

---
+++ scubesolutions/src/main/java/com/example/scubesolutions/ThirdPage.java
@@ -0,0 +1,46 @@
+package com.example.scubesolutions;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ThirdPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_page);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logo:
+                goToUrl("http://www.dominos.ca");
+                break;
+            case R.id.help:
+                goToUrl("https://en.wikipedia.org/wiki/Pizza");
+                break;
+            case R.id.name:
+                recreate();
+                break;
+            case android.R.id.home:
+                recreate();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+}
